5d0e2f22ed46790b7946110140dc9987a4623423
Daniel <dtal@attentigroup.com>
2022-06-06 22:15:01 +0300

    Test-First Approach

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,7 +3,8 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 
-import { errorHandler, NotFoundError } from '@danitaltech-dev/ticketing-common';
+import { currentUser, errorHandler, NotFoundError } from '@danitaltech-dev/ticketing-common';
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -12,6 +13,9 @@ app.use(cookieSession({
   signed: false,
   secure: process.env.NODE_ENV !== 'test'
 }));
+
+app.use(currentUser);
+app.use(createTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
