3b2d45630b8251634d138489f0436ef18ba28319
Daniel <dtal@attentigroup.com>
2022-06-10 13:11:33 +0300

    NATS Streaming Server - An Event Bus Implementation

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,45 @@
+import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto'
+console.clear();
+
+var counter = 0;
+
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
+  url: 'http://localhost:4222',
+});
+
+stan.on('connect', () => {
+  console.log('Listener connected to NATS');
+
+  stan.on('close', () => {
+    console.log('NATS connection closed!');
+    process.exit();
+  });
+
+  const options = stan.subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName('orders-service');
+
+  const subscription = stan.subscribe(
+      'ticket:created',
+      'orders-service-queue-group',
+      options
+      );
+
+  subscription.on('message', (msg: Message) => {
+    console.log('Message recieved');
+    const data = msg.getData();
+    
+    if (typeof data === 'string'){
+        console.log(`Recieved event #${msg.getSequence()}, with data: ${data}`);
+    }
+
+    msg.ack();
+    counter++;
+    console.log(counter);
+  });
+});
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
